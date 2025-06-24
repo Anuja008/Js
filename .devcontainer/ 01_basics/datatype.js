30d24148076c7682a1eab0bdc0c7773fecdcdc0b
Anuja008 <anujadahale08@gmail.com>
2025-06-24 18:54:29 +0000

    datatypes

---
+++ .devcontainer/ 01_basics/datatype.js
@@ -0,0 +1,15 @@
+"use strict";//treat all js code as newer version
+//alert(3=3)//browser me hoga but not here
+console.log(3+3);
+let name="anuja"//string
+let age=3//number
+let isloggedin=false//boolean
+//bigint=for larger number
+//number=2 to power 53
+//boolean
+//null
+//undefined
+//symbol=unique
+//object
+console.log(typeof age)
+console.log(typeof null)
